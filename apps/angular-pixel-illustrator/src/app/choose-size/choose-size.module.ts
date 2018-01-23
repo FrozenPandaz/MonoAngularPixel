0c163ebf87a799e093ab577958bcd9050add874a
CharlieGreenman <abraham.c.greenman@gmail.com>
2018-01-23 10:33:13 -0500

    Add the choose-size component to angular-pixel-illustrator app.

---
+++ apps/angular-pixel-illustrator/src/app/choose-size/choose-size.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChooseSizeComponent } from './choose-size.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ChooseSizeComponent]
+})
+export class ChooseSizeModule {}
