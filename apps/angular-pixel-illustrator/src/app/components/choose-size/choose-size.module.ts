73a472d592e155536474fc523c73acfd71d43807
CharlieGreenman <abraham.c.greenman@gmail.com>
2018-03-23 13:54:48 -0400

    chapter-9: Benchmark progress towards adding a choose-size app component to the app.

--- apps/angular-pixel-illustrator/src/app/components/choose-size/choose-size.module.ts
+++ apps/angular-pixel-illustrator/src/app/components/choose-size/choose-size.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChooseSizeComponent } from './choose-size.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule, MatFormFieldModule,
+    BrowserAnimationsModule, MatCardModule],
   declarations: [ChooseSizeComponent],
   exports: [ChooseSizeComponent]
 })
